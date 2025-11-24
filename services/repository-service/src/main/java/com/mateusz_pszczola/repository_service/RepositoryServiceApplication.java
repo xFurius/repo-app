2ca5ad2ffe7380cc6fa6c1988f5e11e90df1e603
xFurius <matip03211@gmail.com>
2025-11-24 17:45:25 +0100

    starting files

---
+++ services/repository-service/src/main/java/com/mateusz_pszczola/repository_service/RepositoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mateusz_pszczola.repository_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryServiceApplication.class, args);
+	}
+
+}
